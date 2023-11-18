abef4cf3b89f40a481240dca79e044075b1a0fa7
Alex <alexperez689@gmail.com>
2023-11-17 20:27:26 -0500

    Contact bar endpoints added Nav bar started

--- components/ContactBar.vue
+++ components/ContactBar.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="contact__bar">
+  <div class="contact__bar_contain">
     <div class="address__wrap">
-      <h4>29 Hunters Rn Rd, Milford, CT 06460</h4>
+      <h4>
+        <a
+          href="https://maps.app.goo.gl/FbGDrxqqycmP26HM6"
+          target="_blank"
+        >
+          29 Hunters Rn Rd, Milford, CT 06460
+        </a>
+      </h4>
     </div>
     <div class="phone__wrap">
-      <h4>203-878-3818</h4>
+      <h4>
+        <a href="tel:2038783818">
+          203-878-3818
+        </a>
+      </h4>
     </div>
   </div>
 </template>
---
+++ components/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="nav__contain">
+    <div class="nav__wrap">
+      <ul>
+        <li>About</li>
+        <li>Services</li>
+        <li>Projects</li>
+        <li>Contact</li>
+        <li></li>
+      </ul>
+    </div>
+  </div>
+</template>
