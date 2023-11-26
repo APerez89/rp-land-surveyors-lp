de08329a0650c46d3d0838f92fb1633deca2a581
Alex <alexperez689@gmail.com>
2023-11-25 20:46:00 -0500

    new section added to why

--- components/WhyRP.vue
+++ components/WhyRP.vue
@@ -1,17 +1,60 @@
 <template>
   <div id="why__contain" class="main__contain">
     <div class="why__wrap">
-      <div class="img__wrap">
-        <img src="../public/theodolite.png" alt="AI generated image of a thedolite.">
-      </div>
-      <div class="why__info">
-        <h4>Why</h4>
-        <h2>Richard Plain Land Surveyors</h2>
-        <div class="copy">
-          <p>With over 70 years of combined experience Richard Plain, Jr. & Edward Plain make up the <span class="highlight">Full Service Survey Company</span> - Richard Plain Land Surveyors, providing land surveys from mapping & staking to elevation certificates and more, all through Fairfield, New Haven and surrounding counties.</p>
+      <div class="why__top">
+        <div class="img__wrap">
+          <img src="../public/theodolite.png" alt="AI generated image of a thedolite.">
         </div>
-        <div class="button">
-          <Button link="about" message="Learn More" />
+        <div class="why__info">
+          <h4>Why</h4>
+          <h2>Richard Plain Land Surveyors</h2>
+          <div class="copy">
+            <p>With over 70 years of combined experience Richard Plain, Jr. & Edward Plain make up the <span class="highlight">Full Service Survey Company</span> - Richard Plain Land Surveyors, providing land surveys from mapping & staking to elevation certificates and more, all through Fairfield, New Haven and surrounding counties.</p>
+          </div>
+          <div class="button">
+            <Button link="about" message="Learn More" />
+          </div>
+        </div>
+      </div>
+      <div class="why__bottom">
+        <div class="fun__facts">
+          <div class="fact__card">
+            <div class="fact__icon">
+              <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path clip-rule="evenodd" fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0112 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 013.498 1.307 4.491 4.491 0 011.307 3.497A4.49 4.49 0 0121.75 12a4.49 4.49 0 01-1.549 3.397 4.491 4.491 0 01-1.307 3.497 4.491 4.491 0 01-3.497 1.307A4.49 4.49 0 0112 21.75a4.49 4.49 0 01-3.397-1.549 4.49 4.49 0 01-3.498-1.306 4.491 4.491 0 01-1.307-3.498A4.49 4.49 0 012.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 011.307-3.497 4.49 4.49 0 013.497-1.307zm7.007 6.387a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"></path>
+              </svg>
+            </div>
+            <h4>12,986</h4>
+            <h5>Finished Projects</h5>
+          </div>
+          <div class="fact__card">
+            <div class="fact__icon">
+              <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path d="M12.75 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM7.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM8.25 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM9.75 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM10.5 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12.75 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM14.25 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 13.5a.75.75 0 100-1.5.75.75 0 000 1.5z"></path>
+                <path clip-rule="evenodd" fill-rule="evenodd" d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z"></path>
+              </svg>
+            </div>
+            <h4>37</h4>
+            <h5>Years in Business</h5>
+          </div>
+          <div class="fact__card">
+            <div class="fact__icon">
+              <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path clip-rule="evenodd" fill-rule="evenodd" d="M14.615 1.595a.75.75 0 01.359.852L12.982 9.75h7.268a.75.75 0 01.548 1.262l-10.5 11.25a.75.75 0 01-1.272-.71l1.992-7.302H3.75a.75.75 0 01-.548-1.262l10.5-11.25a.75.75 0 01.913-.143z"></path>
+              </svg>
+            </div>
+            <h4>65,700</h4>
+            <h5>Cups of Coffee</h5>
+          </div>
+          <div class="fact__card">
+            <div class="fact__icon">
+              <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path clip-rule="evenodd" fill-rule="evenodd" d="M12 2.25a.75.75 0 01.75.75v.756a49.106 49.106 0 019.152 1 .75.75 0 01-.152 1.485h-1.918l2.474 10.124a.75.75 0 01-.375.84A6.723 6.723 0 0118.75 18a6.723 6.723 0 01-3.181-.795.75.75 0 01-.375-.84l2.474-10.124H12.75v13.28c1.293.076 2.534.343 3.697.776a.75.75 0 01-.262 1.453h-8.37a.75.75 0 01-.262-1.453c1.162-.433 2.404-.7 3.697-.775V6.24H6.332l2.474 10.124a.75.75 0 01-.375.84A6.723 6.723 0 015.25 18a6.723 6.723 0 01-3.181-.795.75.75 0 01-.375-.84L4.168 6.241H2.25a.75.75 0 01-.152-1.485 49.105 49.105 0 019.152-1V3a.75.75 0 01.75-.75zm4.878 13.543l1.872-7.662 1.872 7.662h-3.744zm-9.756 0L5.25 8.131l-1.872 7.662h3.744z"></path>
+              </svg>
+            </div>
+            <h4>0</h4>
+            <h5>Lawsuits</h5>
+          </div>
         </div>
       </div>
     </div>
@@ -22,66 +65,109 @@
 #why__contain {
   background-color: var(--off-white);
   font-family: 'Montserrat', sans-serif;
+  padding: 25px 0px;
 
   .why__wrap {
     max-width: var(--max-width);
     width: 100%;
     display: grid;
-    grid-template-columns: 1fr 1fr;
     gap: 25px;
+    
+    .why__top {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      gap: 25px;
 
-    .img__wrap {
-      width: 100%;
-      position: relative;
-
-      img {
-        width: 130%;
-        position: inherit;
-        transform: rotateY(180deg) rotate(15deg);
-        left: -150px;
-      }
-    }
-
-    .why__info {
-      width: 100%;
-      height: 100%;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-
-      h2 {
-        font-size: 2.3rem;
-        color: var(--not-black);
-        letter-spacing: 3px;
-        padding-bottom: 15px;
-        border-bottom: 2px solid var(--rp-red);
-      }
-      
-      h4 {
-        text-transform: uppercase;
-        letter-spacing: 3px;
-        font-size: 1rem;
-        color: var(--rp-red);
-      }
-
-      .copy {
-        margin-top: 25px;
-
-        p {
-          font-family: 'Raleway', sans-serif;
-          font-size: 1.3rem;
-          color: var(--gray);
-          letter-spacing: 1.1px;
-          line-height: 38px;
+      .img__wrap {
+        width: 100%;
+        position: relative;
+  
+        img {
+          width: 130%;
+          position: inherit;
+          transform: rotateY(180deg) rotate(15deg);
+          left: -150px;
         }
       }
-
-      .button {
+  
+      .why__info {
         width: 100%;
         display: flex;
-        justify-content: flex-end;
+        flex-direction: column;
+        justify-content: center;
+  
+        h2 {
+          font-size: 2.3rem;
+          color: var(--not-black);
+          letter-spacing: 3px;
+          padding-bottom: 15px;
+          border-bottom: 2px solid var(--rp-red);
+        }
+        
+        h4 {
+          text-transform: uppercase;
+          letter-spacing: 3px;
+          font-size: 1rem;
+          color: var(--rp-red);
+        }
+  
+        .copy {
+          margin-top: 25px;
+  
+          p {
+            font-family: 'Raleway', sans-serif;
+            font-size: 1.3rem;
+            color: var(--gray);
+            letter-spacing: 1.1px;
+            line-height: 38px;
+          }
+        }
+  
+        .button {
+          width: 100%;
+          display: flex;
+          justify-content: flex-end;
+        }
       }
     }
+    .why__bottom {
+      width: 100%;
+      padding: 50px 0px;
+
+      .fun__facts {
+        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        gap: 25px;
+        .fact__card {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          background-color: var(--light-white);
+          padding: 25px;
+          box-shadow: 0px 0px 15px rgba(0,0,0,0.1);
+          border-radius: 8px;
+          .fact__icon {
+            width: 45px;
+            color: var(--rp-red);
+            margin-bottom: 5px;
+          }
+
+          h4 {
+            font-size: 3.5rem;
+            font-weight: 500;
+          }
+
+          h5 {
+            font-size: 1.4rem;
+            font-weight: 200;
+            text-transform: uppercase;
+            margin-top: 10px;
+          }
+        }
+      }
+    }
+
   }
 }
 </style>
